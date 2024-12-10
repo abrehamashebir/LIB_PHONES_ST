fdab60875daa5f983c4c73d766c5953c0bf73300
Abreham Ashebir <abrisha96@gmail.com>
2024-12-10 10:54:01 +0000

    adding layout

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ st.markdown(
             background-color: powderblue;
             padding: 10px;
              position: relative;
+             width:300px;
         }
         .nb {
             color: brown;
