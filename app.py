1a2a9fbaeba1a40a5f69a7bb2d52e3bc458a3d25
Abreham Ashebir <abrisha96@gmail.com>
2024-12-10 07:38:43 +0000

    Adding device freindly features

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ try:
 
     # Create HTML with Base64 encoded image
     html = f"""
-        <div style="display: flex; align-items: center; background-color: black;color: white;">
+        <div style="display: block; align-items: center; background-color: black;color: white;">
             <img src="data:image/jpeg;base64,{img_str}" alt="Lion International Bank Logo" style="height: 60px;">
             <h2>Lion International Bank</h2>
             <h4>Phone Clearning </h4>
