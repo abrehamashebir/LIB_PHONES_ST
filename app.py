22bfbb299817bc1fbd4eccf55d6fe7d41dd9b6e4
Abreham Ashebir <abrisha96@gmail.com>
2024-12-10 10:32:41 +0000

    adding layout

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ st.set_page_config(page_title='LIB Customer Phone', layout='wide',menu_items=Non
 st.markdown(
     """
     <style>
-
+    .stElementContainer {
+    position:fixed
+    }
     .gdg-s1dgczr6{
     width: 300px;
     background-color:blue
